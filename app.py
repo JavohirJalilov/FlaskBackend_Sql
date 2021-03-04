bde136aafcf24dec98166a610cc2f991776a033e
Jalilov <jalilovjavohir777@gmail.com>
2021-03-04 09:43:05 +0500

    add user

--- app.py
+++ app.py
@@ -2,12 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(80))
 
 @app.route('/')
 def home():
-    return 'ok'
+    return 'Home Page'
+
+@app.route('/add')
+def add_user():
+    user1 = User(username='Jalilov',email='javohirjalilov777@gmail.com')
+    user2 = User(username='Diyor',email='diyorbekjumanov@gmail.com')
+    db.session.add(user1)
+    db.session.add(user2)
+    bd.session.commit()
+    return f'{type(user1)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
